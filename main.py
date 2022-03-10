742ad52d28183bcc3ed186354d249fd0369d8a02
Twwan <65574497+Twwan@users.noreply.github.com>
2022-03-10 22:52:59 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,37 @@
-# Неборский Мирон 421-4 1.5.1г
+# 1.5.1 г
 import math
 
-x = float(input('Введите значение аргумента x: '))
-eps = float(input('Введите точность вычисления EPS(ε): '))
 
-summand = 1
-n = 3
-y = 0
-while eps < abs(summand):
-    numerator = x**n
-    denominator = math.factorial(n)
-    summand = float(numerator / denominator)
-    y = x + summand
-    n += 2
+def input_x():
+    x = float(input('Введите значение аргумента x: '))
+    return x
 
-print('Ответ:', y)
+
+def input_eps():
+    eps = float(input('Введите точность вычисления EPS(ε): '))
+    return eps
+
+
+def answer(x, eps):
+    summand = 1
+    n = 3
+    y = 0
+
+    while eps < abs(summand):
+        numerator = x**n
+        denominator = math.factorial(n)
+        summand = float(numerator / denominator)
+        y = x + summand
+        n += 2
+
+    print('Ответ:', y)
+
+
+def main():
+    x = input_x()
+    eps = input_eps()
+    y = answer(x, eps)
+
+
+if __name__ == "__main__":
+    main()
